53462df5e05f7fe2a30f4bf97c14a48158efbea2
Xiao Pu <acesscp@gmail.com>
2016-05-23 22:49:43 +0800

    message model initialize

---
+++ database/migrations/2016_05_23_092144_create_conversations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConversationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('to_user')->unsigned();
+            $table->integer('from_user')->unsigned();
+            $table->boolean('can_reply');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('conversations');
+    }
+}
---
+++ database/migrations/2016_05_23_092652_create_messages_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->text('content');
+            
+            $table->boolean('has_read');
+
+            $table->integer('conversation_id');
+            $table->foreign('conversation_id')
+                ->references('id')
+                ->on('conversations')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('messages');
+    }
+}
