b2b2afeb5bcffcb4fd1e3ad73c66724fb14109c6
Elijahsk <song.kai@dhs.sg>
2016-07-24 18:51:07 +0800

    Update footer information

--- resources/views/partials/_site_copyright.blade.php
+++ resources/views/partials/_site_copyright.blade.php
@@ -1,11 +1,11 @@
 <div class="sitesCopyright">
     <div class="sitesCopyrightInner">
         <ul>
-            <li><a href="#">Guidlines</a></li>
-            <li><a href="#">Suggestion</a></li>
-            <li><a href="#">Join us</a></li>
-            <li><a href="#">Terms & Condition</a></li>
-            <li><a href="#">Contact us</a></li>
+            <li><a href="http://blog.nusding.info/index.php/2016/07/23/how-to-use-nusding/">Guidlines</a></li>
+            <li><a href="http://blog.nusding.info/index.php/2016/07/21/contact-us/">Suggestions</a></li>
+            <li><a href="https://github.com/xpdavid/Ding">Join us</a></li>
+            <li><a href="http://blog.nusding.info/index.php/2016/07/23/terms-and-conditions/">Terms & Conditions</a></li>
+            <li><a href="http://blog.nusding.info/index.php/2016/07/21/contact-us/">Contact us</a></li>
         </ul>
         <div> &copy 2016 Ding & KKP All Rights Reserved</div>
     </div>
