1199b04db96360529e4bd1b4f1fd2f92e0b1f2f8
Xiao Pu <acesscp@gmail.com>
2016-07-25 22:02:42 +0800

    update bookmark UI / site copyright UI

--- resources/views/partials/_site_copyright.blade.php
+++ resources/views/partials/_site_copyright.blade.php
@@ -4,7 +4,7 @@
             <li><a href="http://blog.nusding.info/index.php/2016/07/23/how-to-use-nusding/">Guidlines</a></li>
             <li><a href="http://blog.nusding.info/index.php/2016/07/21/contact-us/">Suggestions</a></li>
             <li><a href="https://github.com/xpdavid/Ding">Join us</a></li>
-            <li><a href="http://blog.nusding.info/index.php/2016/07/23/terms-and-conditions/">Terms & Conditions</a></li>
+            <li><a href="http://blog.nusding.info/index.php/2016/07/12/terms-and-conditions/">Terms & Conditions</a></li>
             <li><a href="http://blog.nusding.info/index.php/2016/07/21/contact-us/">Contact us</a></li>
         </ul>
         <div> &copy 2016 Ding & KKP All Rights Reserved</div>
